e66c2fb505b71ae38c2952d422f8ab8975090353
raydecastro <captray108@gmail.com>
2016-10-06 12:11:17 -0700

    feature. upgraded project scaffold.

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# flower-of-life
+# Flower of Life
+
+## install
+```
+npm install
+```
+
+## test
+```
+npm test
+```
+
+## technologies
+* javascript
+* es6 + babel
+* mocha + chai
+* node + npm 
+* gulp
+
+## license
+MIT
---
+++ src/app.js
@@ -0,0 +1,14 @@
+"use strict";
+
+class App {
+    constructor() {
+        this.love = "the most powerful force in the universe.";
+    }
+
+    run() {
+        console.log(".love> is " + this.love);
+        return true; 
+    }
+}
+
+export default App;
